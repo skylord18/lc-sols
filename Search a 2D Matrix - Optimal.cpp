dd47203ff9185714cd424dc2edaa0d078a5a2ed3
Bhaskar Verma <89547408+skylord18@users.noreply.github.com>
2022-04-23 20:17:35 +0530

    Add files via upload

---
+++ Search a 2D Matrix - Optimal.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int low = 0, high = m*n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(matrix[mid/m][mid%m]==target)
+                return true;
+            else if(matrix[mid/m][mid%m]>target)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return false;
+        
+        
+    }
+};
